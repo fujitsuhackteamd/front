d8be0ac7fe4a5654b53b7084e233e5b3b87f6e74
KatoYugo <kato.1999.cs@gmail.com>
2020-12-05 00:08:47 +0900

    入力フォームの完成

--- develop/main.py
+++ develop/main.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='./load', template_folder='./html_file')
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+
+# ログインフォームのテスト
+@app.route('/login_result', methods=['GET', 'POST'])
+def login_result():
+    return "email：" + request.form["email"] + "　　pass:" + request.form["password"]
+
+
+@app.route('/form.html')
+def form():
+    return render_template('form.html')
+
+@app.route('/form_result', methods=['GET', 'POST'])
+def form_result():
+    return "email：" + request.form["age"] 
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
